71687b1b3796caa1fb3dad3a02950e792a8cd2a3
Fu Hanxi <fuhanxi@espressif.com>
2022-06-20 14:29:30 +0800

    chore: style files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # idf-build-apps
 
+## Contributing
+
+For 2.7, 3.4(?), 3.5(?), 3.6-3.10 compatibility, we use type annotation in comments.
---
+++ idf_build_apps/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Tools for building ESP-IDF related apps.
+"""
+
+__version__ = '0.1.0'
