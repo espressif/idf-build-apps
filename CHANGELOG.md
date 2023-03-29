47a7dffd2f47da2998122e7b2dbcf0a8fd832999
Fu Hanxi <fuhanxi@espressif.com>
2023-03-29 12:27:21 +0800

    bump: 0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## [Unreleased]
+## [0.5.0] (2023-03-29)
 
 ### Added
 
--- idf_build_apps/__init__.py
+++ idf_build_apps/__init__.py
@@ -7,7 +7,7 @@ Tools for building ESP-IDF related apps.
 
 import logging
 
-__version__ = '0.5.0.rc0'
+__version__ = '0.5.0'
 
 LOGGER = logging.getLogger('idf_build_apps')
 
