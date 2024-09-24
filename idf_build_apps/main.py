97b85f5c2755a9b6ecf2c5e7516751651588e06b
Fu Hanxi <fuhanxi@espressif.com>
2024-09-24 10:15:28 +0200

    fix: compatible with earlier versions
    also add missing docstrings

--- idf_build_apps/main.py
+++ idf_build_apps/main.py
@@ -70,6 +70,15 @@ def find_apps(
     """
     apply_config_file(config_file)
 
+    # compatible with old usage
+    ## `preserve`
+    if 'preserve' in kwargs:
+        LOGGER.warning(
+            'Passing "preserve" directly is deprecated. '
+            'Pass "no_preserve" instead to disable preserving the build directory'
+        )
+        kwargs['no_preserve'] = not kwargs.pop('preserve')
+
     if find_arguments is None:
         find_arguments = FindArguments(
             paths=to_list(paths),  # type: ignore
@@ -125,7 +134,8 @@ def build_apps(
     """
     apply_config_file(config_file)
 
-    apps = to_list(apps)
+    # compatible with old usage
+    ## `check_app_dependencies`
     if 'check_app_dependencies' in kwargs:
         LOGGER.warning(
             'Passing "check_app_dependencies" directly is deprecated. '
@@ -138,6 +148,7 @@ def build_apps(
             **kwargs,
         )
 
+    apps = to_list(apps)
     if apps is None:
         apps = find_apps(
             find_arguments=FindArguments(
--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -10,13 +10,6 @@ from idf_build_apps.main import main
 from idf_build_apps.utils import InvalidCommand
 
 
-class FakeArgs:
-    """like what argparse.Namespace does"""
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-
 @pytest.mark.parametrize(
     'args, expected_error',
     [
